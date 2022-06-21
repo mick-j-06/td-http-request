8ab343fca24c7fe9d5ad86b1b2593f6f5ad4ce99
Micael Jerry <hei.jerry.2@gmail.com>
2022-06-21 05:43:57 +0300

    getter

--- src/App.js
+++ src/App.js
@@ -1,48 +1,49 @@
 import "./App.css";
-import { useState } from "react";
+import {useState} from "react";
 import axios from "axios";
 
 function App() {
-  let [result, setResult] = useState([]);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          const promise = axios.get("http://localhost:3000/employee");
-          promise
-            .then((response) => {
-              console.log(response);
-              setResult(response.data);
-            })
-            .catch((error) => {
-              console.error(error);
-            });
-        }}
-      >
-        Get employees
-      </button>
-      <button>Add</button>
-      <button>Update</button>
-      {result.length > 0 && (
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Position</th>
-            </tr>
-          </thead>
-          <tbody>
-            {result.map((item) => (
-              <tr key={`${item.id}-${item.name}`}>
-                <td>{item.name}</td>
-                <td>{item.position}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
-  );
+    let [result, setResult] = useState([]);
+    return (
+        <div>
+            <button
+                onClick={() => {
+                    const promise = axios.get("https://jsonplaceholder.typicode.com/posts");
+                    promise
+                        .then((response) => {
+                            console.log(response);
+                            setResult(response.data);
+                        })
+                        .catch((error) => {
+                            console.error(error);
+                        });
+                }}
+            >
+                Get employees
+            </button>
+            <button>Add</button>
+            <button>Update</button>
+            {result.length > 0 && (
+                <table>
+                    <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>title</th>
+                        <th>body</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {result.map((item) => (
+                        <tr key={`${item.id}-${item.title}`}>
+                            <td>{item.id}</td>
+                            <td>{item.title}</td>
+                            <td>{item.body}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            )}
+        </div>
+    );
 }
-
 export default App;
